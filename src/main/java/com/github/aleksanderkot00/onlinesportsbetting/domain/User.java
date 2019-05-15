a880eb4fe2c2e85b2422ecc004227fd5f482ecdb
unknown <aleksanderkot00@gmail.com>
2019-05-15 20:46:47 +0200

    User class - first implementation

---
+++ src/main/java/com/github/aleksanderkot00/onlinesportsbetting/domain/User.java
@@ -0,0 +1,11 @@
+package com.github.aleksanderkot00.onlinesportsbetting.domain;
+
+import java.math.BigDecimal;
+
+public class User {
+    private long userId;
+    private String name;
+    private String lastName;
+    private String mailAddress;
+    private BigDecimal balance;
+}
