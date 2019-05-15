3b413f92ce493831ea9a1a6cee3a71f995cc0539
unknown <aleksanderkot00@gmail.com>
2019-05-15 21:11:06 +0200

    FootballMarch, FootballBet classes - first implementation

---
+++ src/main/java/com/github/aleksanderkot00/onlinesportsbetting/domain/FootballBet.java
@@ -0,0 +1,11 @@
+package com.github.aleksanderkot00.onlinesportsbetting.domain;
+
+import java.math.BigDecimal;
+
+public class FootballBet {
+    private long betId;
+    private FootballMatch match;
+    private BigDecimal odd1;
+    private BigDecimal odd2;
+    private BigDecimal oddX;
+}
