7901ad80682673a89b9b40e73ad6fd4cb22c70b7
unknown <aleksanderkot00@gmail.com>
2019-06-02 18:21:26 +0200

    Implementation od User entity

--- src/main/java/com/github/aleksanderkot00/onlinesportsbetting/domain/User.java
+++ src/main/java/com/github/aleksanderkot00/onlinesportsbetting/domain/User.java
@@ -1,11 +1,69 @@
 package com.github.aleksanderkot00.onlinesportsbetting.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "USER_ID")
     private long userId;
+
+    @NotNull
+    @Size(min = 2, max = 12)
     private String name;
+
+    @NotNull
+    @Size(min = 2, max = 12)
     private String lastName;
-    private String mailAddress;
+
+    @NotNull
+    @Email
+    private String email;
     private BigDecimal balance;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
