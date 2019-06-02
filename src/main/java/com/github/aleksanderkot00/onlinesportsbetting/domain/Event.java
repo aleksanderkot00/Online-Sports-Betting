ce8856e5f409189fc6b67f842e957af71ac46bca
unknown <aleksanderkot00@gmail.com>
2019-06-02 22:03:06 +0200

    Event entity implementation

--- src/main/java/com/github/aleksanderkot00/onlinesportsbetting/domain/Event.java
+++ src/main/java/com/github/aleksanderkot00/onlinesportsbetting/domain/Event.java
@@ -1,12 +1,30 @@
 package com.github.aleksanderkot00.onlinesportsbetting.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.Set;
 
+@Entity(name = "EVENTS")
 public class Event {
+
+    @NotNull
+    @Id
+    @GeneratedValue
+    private long eventId;
+
+    @NotNull
     private LocalDateTime dateTime;
+
+    @NotNull
+    @Size(min = 2, max = 25)
     private String teamOneName;
+
+    @NotNull
+    @Size(min = 2, max = 25)
     private String teamTwoName;
-    private Set<Results> possibleResults;
+
     private Results result;
 }
