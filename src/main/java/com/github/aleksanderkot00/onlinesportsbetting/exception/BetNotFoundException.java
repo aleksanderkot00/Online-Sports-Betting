c29e9b97083bfb6cd12942376c34001aff03b58b
unknown <aleksanderkot00@gmail.com>
2019-06-03 23:34:09 +0200

    Correction of Exceptions

--- src/main/java/com/github/aleksanderkot00/onlinesportsbetting/exception/BetNotFoundException.java
+++ src/main/java/com/github/aleksanderkot00/onlinesportsbetting/exception/BetNotFoundException.java
@@ -1,4 +1,8 @@
 package com.github.aleksanderkot00.onlinesportsbetting.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BetNotFoundException extends RuntimeException {
 }
