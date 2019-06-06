f0c026c9682fff089c51cf98ed93a613bcb6df4d
unknown <aleksanderkot00@gmail.com>
2019-06-06 16:17:25 +0200

    Addition of addBetToSlip and removeBetFromBetSlip methods in BetSlipService

--- src/main/java/com/github/aleksanderkot00/onlinesportsbetting/service/BetSlipService.java
+++ src/main/java/com/github/aleksanderkot00/onlinesportsbetting/service/BetSlipService.java
@@ -1,7 +1,10 @@
 package com.github.aleksanderkot00.onlinesportsbetting.service;
 
+import com.github.aleksanderkot00.onlinesportsbetting.domain.Bet;
 import com.github.aleksanderkot00.onlinesportsbetting.domain.BetSlip;
+import com.github.aleksanderkot00.onlinesportsbetting.exception.BetNotFoundException;
 import com.github.aleksanderkot00.onlinesportsbetting.exception.BetSlipNotFoundException;
+import com.github.aleksanderkot00.onlinesportsbetting.repository.BetRepository;
 import com.github.aleksanderkot00.onlinesportsbetting.repository.BetSlipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,9 @@ public class BetSlipService {
 
     @Autowired
     private BetSlipRepository betSlipRepository;
+
+    @Autowired
+    private BetRepository betRepository;
 
     public List<BetSlip> getBetSlips() {
         return betSlipRepository.findAll();
@@ -28,5 +34,19 @@ public class BetSlipService {
 
     public void deleteBetSlip(long betSlipId) {
         betSlipRepository.deleteById(betSlipId);
+    }
+
+    public BetSlip addBetToSlip(long betSlipId, long betId) {
+        BetSlip betSlip = betSlipRepository.findById(betSlipId).orElseThrow(BetSlipNotFoundException::new);
+        Bet bet = betRepository.findById(betId).orElseThrow(BetNotFoundException::new);
+        betSlip.getBets().add(bet);
+        return betSlipRepository.save(betSlip);
+    }
+
+    public BetSlip removeBetFromSlip(long betSlipId, long betId) {
+        BetSlip betSlip = betSlipRepository.findById(betSlipId).orElseThrow(BetSlipNotFoundException::new);
+        Bet bet = betRepository.findById(betId).orElseThrow(BetNotFoundException::new);
+        betSlip.getBets().remove(bet);
+        return betSlipRepository.save(betSlip);
     }
 }
