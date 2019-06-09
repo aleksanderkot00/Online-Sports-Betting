565ad1f3b10780c0a85e7049826981461abcd046
unknown <aleksanderkot00@gmail.com>
2019-06-09 17:36:02 +0200

    implementation of FootballMatchDto class

--- src/main/java/com/github/aleksanderkot00/onlinesportsbetting/api/football/FootballMatchDto.java
+++ src/main/java/com/github/aleksanderkot00/onlinesportsbetting/api/football/FootballMatchDto.java
@@ -1,4 +1,37 @@
 package com.github.aleksanderkot00.onlinesportsbetting.api.football;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FootballMatchDto {
+
+    @JsonProperty("country_name")
+    private String country;
+
+    @JsonProperty("league_name")
+    private String league;
+
+    @JsonProperty("match_date")
+    private LocalDate date;
+
+    @JsonProperty("match_hometeam_name")
+    private String teamOneName;
+
+    @JsonProperty("match_hometeam_score")
+    private int teamOneScore;
+
+    @JsonProperty("match_awayteam_name")
+    private String teamTwoName;
+
+    @JsonProperty("match_awayteam_score")
+    private int teamTwoScore;
 }
